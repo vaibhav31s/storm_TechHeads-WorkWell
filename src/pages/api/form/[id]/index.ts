878cdc9eb4056be6e497c16c833e6a6287451cac
TanishSuvarna <stanish.mi@gmail.com>
2023-10-04 17:00:13 +0530

    form creation

--- src/pages/api/form/[id]/index.ts
+++ src/pages/api/form/[id]/index.ts
@@ -1,15 +1,48 @@
 import { NextApiRequest, NextApiResponse } from "next";
-// import { prisma } from "../../../utils/db";
+import { prisma } from "../../../../utils/db";
 
-const createUser = async (req: NextApiRequest, res: NextApiResponse) => {
-  try {
-    res.status(200).json("user")  
-} catch (err) {
-    console.log(err);
-    res.status(500).json({ message: "Internal server error" });
-  }
-};
 
+
+
+const submit = async (req: NextApiRequest, res: NextApiResponse) => {
+    try {
+      const formId :any  = req.query.id;
+      const { userId, questionIds, responses } = req.body;
+      
+      
+      // Loop through questionIds and responses arrays
+      for (let i = 0; i < questionIds.length; i++) {
+        const questionId = questionIds[i];
+        const responseText = responses[i]; // Assuming responses array matches questionIds
+      
+        // Create a response record for each question
+        await prisma.response.create({
+          data: {
+            form: {connect: { id : formId }}, // Assuming you have formId
+            user: {connect: { id : userId }},
+            question: {connect: { id : questionId }},
+            answer: responseText,
+          },
+        });
+      }
+      await prisma.user.update({
+        where: {
+          id: userId // Provide the specific userId to identify the user you want to update
+        },
+        data: {
+          form: {
+            connect: {
+              id: formId // Provide the new formId you want to associate with the user
+            }
+          }
+        }
+      });
+      res.status(200).json({ message: "Responses created successfully" });
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ message: "Internal server error" });
+    }
+  };
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
@@ -17,10 +50,10 @@ export default async function handler(
   const method = req.method;
   switch (method) {
     case "GET":
-        createUser(req, res);
+      
       break;
     case "POST":
-      
+      submit(req , res);
       break;
     case "PATCH":
       break;
--- src/pages/api/form/index.ts
+++ src/pages/api/form/index.ts
@@ -1,11 +1,35 @@
 import { NextApiRequest, NextApiResponse } from "next";
-// import { prisma } from "../../../utils/db";
+import { prisma } from "../../../utils/db";
 
-const hello = async (req: NextApiRequest, res: NextApiResponse) => {
+const formGen = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
-    const { name, email, avatar } = req.body;
-    
-    res.status(200).json("user");
+    // Creating Forms //
+    const {userId , title , description , questions ,cc} = req.body;
+
+    const createdForm = await prisma.form.create({
+      data: {
+        title: title,
+        description: description,
+        cc: cc,
+      }
+    });
+
+    console.log(createdForm);
+
+    for(let ques of questions){
+      await prisma.question.create({data:{
+        title:ques.title,
+        type:ques.type,
+        options:ques.options,
+        form : {
+          connect:{
+            id : createdForm.id
+          }
+        }
+      }})
+    }
+
+    res.status(200).json({message:"Form Created"});
   } catch (err) {
     console.log(err);
     res.status(500).json({ message: "Internal server error" });
@@ -21,7 +45,7 @@ export default async function handler(
     case "GET":
       break;
     case "POST":
-      hello(req, res);
+      formGen(req, res);
       break;
     case "PATCH":
       break;
