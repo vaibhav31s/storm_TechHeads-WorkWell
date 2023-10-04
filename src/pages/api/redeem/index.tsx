f879007c09896b4f0513eba767880afdd2b2ab8c
TanishSuvarna <stanish.mi@gmail.com>
2023-10-05 05:14:29 +0530

    redeem

---
+++ src/pages/api/redeem/index.tsx
@@ -0,0 +1,46 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { prisma } from "../../../utils/db";
+
+
+const data = async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    
+    const cost : any = req.body.cost;
+    const userId : any = req.body.userId;
+    await prisma.user.update({
+        where:{
+            id : userId
+        },
+        data:{
+            points:{
+                decrement : cost
+            }
+        }
+    }) 
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const method = req.method;
+  switch (method) {
+    case "GET":
+       
+      break;
+    case "POST":
+        data(req, res);
+      break;
+    case "PATCH":
+      break;
+    case "DELETE":
+      break;
+    default:
+      res.status(500).json({ message: "Method not allowed" });
+      break;
+  }
+}
