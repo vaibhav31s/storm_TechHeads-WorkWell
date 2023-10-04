d6e3763412ae398926b6d0a96094400839839d70
Jeet Rane <jeetrane2002rj@gmail.com>
2023-10-04 17:09:13 +0530

    inprocess chart

--- Components/Chart/BarChart.tsx
+++ Components/Chart/BarChart.tsx
@@ -1,7 +1,23 @@
 import React from 'react'
-import {Chart} from 'react-apexcharts';
+import Chart from 'react-apexcharts';
 
 const BarChart = () => {
+  const state = {
+    options: {
+      chart: {
+        id: "basic-bar"
+      },
+      xaxis: {
+        categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999]
+      }
+    },
+    series: [
+      {
+        name: "series-1",
+        data: [30, 40, 45, 50, 49, 60, 70, 91]
+      }
+    ]
+  };
   return (
     <div>BarChart</div>
   )
