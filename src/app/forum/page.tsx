2937d9ebf2b0f52f742c13b407b3829b0fa5c88a
vaibhav31s <gawadvaibhavv@gmail.com>
2023-10-05 08:43:00 +0530

    adfasdf

--- src/app/forum/page.tsx
+++ src/app/forum/page.tsx
@@ -4,7 +4,7 @@ type Props = {}
 
 const page = (props: Props) => {
   return (
-    <div className='flex flex-col space-y-4 p-16'>
+    <div className='dark:text-white flex flex-col space-y-4 p-16'>
         <h1 className='font-bold text-xl'>Engagement Forum : </h1>
        <Cardss/>
        <Cardss/>
